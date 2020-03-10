726cd7df62322e7f4022c921bb35015e21fd3795
eduardo <eduardozaffarimonteiro@gmail.com>
2020-03-10 20:32:31 -0300

    arrumando uma cagada

--- vagapcd/App.js
+++ vagapcd/App.js
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-export default class login extends Component {
+export default class Login extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>VAGA PCD</Text>
-        <form method="post" action="">
-          <label for="email">E-mail</label>
-            <input type="text" id="email"/>
-          <label for="senha">Senha</label>
-            <input type="password" id="senha"/>
-            <input type="submit" id="entrar"/>
-        </form>
         
       </View>
     );
