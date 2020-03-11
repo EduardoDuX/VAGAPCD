870ec3b33b4b5b757dcae75ed8994deefd6ad240
eduardo <eduardozaffarimonteiro@gmail.com>
2020-03-11 09:37:10 -0300

    mudanças 11/03

--- vagapcd/App.js
+++ vagapcd/App.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+
+const styles = StyleSheet.create({
+
+  bigBlack: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 30,
+    alignItems:'center',
+    flex:1,
+    justifyContent:'center',
+    
+  },
+  black:{
+    color:'black',
+  },
+  });
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>VAGA PCD</Text>
+      <View style={styles.bigBlack}>
+      <Text>VAGA PCD:{this.state.text}  </Text>
         
+      <TextInput
+          placeholder="E-mail"
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+      />
       </View>
-    );
+    )
   }
 }
+
+
+
+
+
