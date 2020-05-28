277417f9215e7832781e3c6aece6b68e8bd8c274
Jonas <57230582+JonasFonM@users.noreply.github.com>
2020-05-28 16:04:42 -0300

    Tela agora com anotações
    Só tem uma anotação agora, mas pelo menos sabemos como fazer mais

--- vagapcd/components/TelaMapa.js
+++ vagapcd/components/TelaMapa.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image, Platform } from 'react-native';
 import {Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text}
 from 'native-base';
-import MapboxGL from "@react-native-mapbox-gl/maps";
+import MapboxGL from '@mapbox/react-native-mapbox-gl';
 
-
+//Não esqueçam o AccessToken pra testar ;) 
 MapboxGL.setAccessToken("");
 MapboxGL.setConnected(true);
-
 
 const styles = StyleSheet.create({
   page: {
@@ -23,11 +22,43 @@ const styles = StyleSheet.create({
     height: 500,
     width: 600,
     flex: 1
-  }
+  }, container: {
+    flex: 1,
+  },
+
+  //Estilos dos pins(Ainda não consegui usar um .png em cima do mapa, Jojo)
+  annotationContainer: {
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+    borderRadius: 15,},
+  
+  annotationFill: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: '#005EFF',
+    transform: [{ scale: 0.8 }],}
 });
 
-export default class TelaMapa extends React.Component {
-
+  export default class TelaMapa extends Component {
+    // Renderização dos 'pins' no mapa, Jojo
+    renderAnnotations() {
+      return (
+        <MapboxGL.PointAnnotation
+          id='Xanxerê'
+          coordinate={[-52.4076058,-26.8737824]}
+        >
+          <View style={styles.annotationContainer}>
+            <View style={styles.annotationFill} />
+          </View>
+          <MapboxGL.Callout title='Xanxerê' />
+        </MapboxGL.PointAnnotation>
+      )
+    }
+    
   constructor(props) {
     super(props);
     this.state = {
@@ -45,20 +76,18 @@ export default class TelaMapa extends React.Component {
     this.setState({ isReady: true });
   }
 
-  render() {
-    return (
-      <Container>
 
-        
-
-        <View style={styles.page}>
-          <View stule={styles.container}>
-            <MapboxGL.MapView style={styles.map}/>
-          </View>
-        </View>
-
-
-      </Container>
-    );
-  }
+    render() {
+      return (
+        // Renderização do mapa (coordenadas centrais e local do usuário não funcionais no momento)
+        <MapboxGL.MapView
+          centerCoordinate={[-52.4076058,-26.8737824]}
+          style={styles.container}
+          showUserLocation
+          styleURL={MapboxGL.StyleURL.map}
+        >
+        {this.renderAnnotations()}
+        </MapboxGL.MapView>
+      );
+    }   
 }
